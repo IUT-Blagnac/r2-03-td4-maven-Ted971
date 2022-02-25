68e1cc5eb4d632bae0476a03b22dcc0fec1c0cb9
BABEL Teddy <94467207+Ted971@users.noreply.github.com>
2022-02-25 15:26:50 +0100

    q4

--- src/main/java/Matrice.java
+++ src/main/java/Matrice.java
@@ -1,30 +1,49 @@
 
 public class Matrice
-{
+{    /**
+    *place une étoile
+     */
     public static void star(){
         System.out.print("*");
     }
-
+    
+     /**
+    *deplace de un sur la droite
+     */
     public static void right(){
         System.out.print(" ");
     }
-
+     /**
+    *passe à la prochaine ligne
+     */
     public static void next(){
         System.out.print("\n");
     }
 
+     /**
+    *place une étoile
+    *@param n le nombre d'étoiles qu'on veut placer
+     */
     public static void n_star(int n){
         int i;
         for (i=0; i<n ; i=i+1)
             star();
     }
 
+    /**
+    *deplace de n sur la droite
+    *@param n le nombre de fois qu'on veut se déplacer
+     */
     public static void n_right(int n){
         int i;
         for (i=0; i<n ; i=i+1)
             right();
     }
 
+    /**
+    *genere un sapin plein
+    *@param h la taille du sapin
+     */
     public static void genererSapinPlein(int h){
         int l; int nbR=h-1; int nbS=1;
         for (l=1 ; l<=h ; l=l+1){
@@ -36,6 +55,10 @@ public class Matrice
         }
     }
 
+    /**
+    *genere un sapin vide
+    *@param h la taille du sapin
+     */
     public static void genererSapinVide(int h){
         int l; int nbR=h-1; int nbS=1; int nbEspace = 1;
         n_right(nbR);
@@ -55,6 +78,11 @@ public class Matrice
         n_star(nbEspace);
     }
 
+    /**
+    * genere un String en ligne 
+    *@param l c'est la taille de la ligne
+    *@return une ligne de taille l
+     */
     public static String genererStringLigne(int l){
         String ligne="";
         int i;
@@ -64,6 +92,7 @@ public class Matrice
         ligne=ligne+("\n");
         return ligne;
     }
+
     /**
     *vraiment chiant mais génère un sapin couché
     *@param h un int c'est la taille de ton sapin
@@ -80,10 +109,12 @@ public class Matrice
         }
         return sapin;
     }
+
     /**
     *dessine un sapin couché
     *c'est tout
     *sou-bis
+    *@param args aucune idée mais ans lui tout meurs
      */
     public static void main(String[] args){
         System.out.println(genererSapinCouche(5));
